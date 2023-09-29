b21374b685fee98ff6d87b791891f4fa75c02f01
Valera <99064072+YaganovValera@users.noreply.github.com>
2023-09-29 12:31:03 +0400

    end_project

--- logger.py
+++ logger.py
@@ -1,19 +1,16 @@
 import os
 
+
 def input_contact():
-    # f = open('data.txt', 'r')
-    # if not f:
-    #     f = open('data.txt', 'w')
-    #     f.close()
-    # else:
-    #     f.close()
     if not os.path.isfile('data.txt'):
         f = open('data.txt', 'w')
         f.close()
 
-
     with open('data.txt', 'a', encoding='utf-8') as f:
         user = input('Введите имя, фамилию и телефон: ').strip().split()
+        if len(user) != 3:
+            print("Ошибка ввода данных!")
+            return input_contact()
         f.write(';'.join(user) + '\n')
 
 
@@ -29,7 +26,11 @@ def find_contact():
         contacts = f.readlines()
     while True:
         print('По каким параметрам ищем контакт?:\n1. Имя\n2. Фамилия\n3. Телефон')
-        command_index = int(input('Команда: '))
+        try:
+            command_index = int(input('Команда: '))
+        except ValueError:
+            print("Неверный ввод данных!")
+            continue
         if str(command_index) not in '123':
             print('Других параметров нету.')
         else:
@@ -42,3 +43,78 @@ def find_contact():
             print(' '.join(full_contact))
 
 
+def change_contact():
+    try:
+        with open('data.txt', 'r+', encoding='utf-8') as f:
+            contacts = f.readlines()
+
+            user = input(
+                'Введите имя, фамилию и телефон пользователя, чьи данные хотите изменить (через пробел): ').strip().split()
+            if len(user) != 3:
+                print("Ошибка ввода данных!")
+                return
+
+            # Проверяем, существует ли пользователь
+            if not (';'.join(user) + '\n') in contacts:
+                print("Пользователь не существует!")
+                return
+
+            while True:
+                print('Какой параметр хотите изменить?:\n1. Имя\n2. Фамилия\n3. Телефон')
+                try:
+                    command_index = int(input('Команда: '))
+                except ValueError:
+                    print("Неверный ввод данных!")
+                    continue
+                if command_index not in [1, 2, 3]:
+                    print('Других параметров нет.')
+                else:
+                    break
+
+            data = input('Введите новые данные: ')
+
+            # Обновляем информацию в списке контактов
+            for i in range(len(contacts)):
+                full_contact = contacts[i].strip().split(';')
+                if user == full_contact:
+                    full_contact[command_index - 1] = data
+                    contacts[i] = ';'.join(full_contact) + '\n'
+                    break
+
+        # Перезаписываем файл с обновленными данными
+        with open('data.txt', 'w', encoding='utf-8') as f:
+            f.writelines(contacts)
+
+        print('Результат обновления: Данные успешно изменены.')
+
+    except FileNotFoundError:
+        print("Файл 'data.txt' не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}")
+
+
+def delete_contact():
+    try:
+        with open('data.txt', 'r', encoding='utf-8') as f:
+            contacts = f.readlines()
+
+            user = input('Введите имя, фамилию и телефон пользователя, чью запись хотите удалить (через пробел): ').strip().split()
+            if len(user) != 3:
+                print("Ошибка ввода данных!")
+                return
+
+            # Проверяем, существует ли пользователь
+            if not (';'.join(user) + '\n') in contacts:
+                return print("Пользователь не существует!")
+
+            # Удаляем строку из списка
+            del contacts[contacts.index(';'.join(user) + '\n')]
+
+        # Теперь перезаписываем файл без удаленной строки
+        with open('data.txt', 'w', encoding='utf-8') as f:
+            f.writelines(contacts)
+
+    except FileNotFoundError:
+        print("Файл 'data.txt' не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from logger import input_contact, print_contacts, find_contact
+from logger import *
 
 
 def menu():
@@ -7,16 +7,25 @@ def menu():
            "1. Добавить контакт\n" \
            "2. Посмотреть все контакты\n" \
            "3. Найти контакт\n"\
-           "4. "
+           "4. Изменить контакт\n"\
+           "5. Удалить контакт\n"
     print(text)
     while True:
-        comand = int(input('Введите команду: \n'))
+        try:
+            comand = int(input('Введите команду: '))
+        except ValueError:
+            continue
+
         if comand == 1:
             input_contact()
         if comand == 2:
             print_contacts()
         if comand == 3:
             find_contact()
+        if comand == 4:
+            change_contact()
+        if comand == 5:
+            delete_contact()
         if comand == 0:
             break
         print('_' * 30)
